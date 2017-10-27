d378d11c29a7593538ce2283e923c7e0eceaf5e9
natinusala <natinusala@gmail.com>
2017-10-27 10:34:28 +0200

    added releases

--- create_sql.py
+++ create_sql.py
@@ -17,6 +17,8 @@ REPO_REPO = ""
 DATA = {}
 OAUTH_TOKEN = ""
 
+# TODO Ability to process multiple repositories
+
 
 # All pull requests are issues and not all issues are pull requests
 # and the issues API will not return all pull requests, only those created after a certain date
@@ -25,12 +27,13 @@ def populate_issues(endpoint, name, exclude_pr):
     page = 0
 
     while True:  # do-while loop
-        print("Getting page " + str(page+1))
+        print("Loading page " + str(page+1))
         issues = call_github_api("/" + endpoint + "?page=" + str(page) + "&state=all")
 
         if len(issues) <= 0:
             break
 
+        added_issues = 0
         for issue in issues:
             print("Processing issue " + str(issue["id"]))
 
@@ -40,6 +43,8 @@ def populate_issues(endpoint, name, exclude_pr):
 
             opened_count = 0
             closed_count = 0
+
+            added_issues = added_issues + 1
 
             for week in DATA["weeks"]:
                 issue_created_dt = dateutil.parser.parse(issue["created_at"])
@@ -73,7 +78,7 @@ def populate_issues(endpoint, name, exclude_pr):
             else:
                 print("Issue added " + str(count) + " times, " + str(opened_count) + " times as an open issue and " + str(closed_count) + " times as a closed issue")
 
-        print("Added " + str(len(issue)) + " issues for page " + str(page+1))
+        print("Added " + str(added_issues) + " issues for page " + str(page+1))
 
         page = page + 1
 
@@ -144,12 +149,21 @@ if __name__ == "__main__":
         DATA["weeks"][week_timestamp]["issues"]["open"] = 0
         DATA["weeks"][week_timestamp]["issues"]["closed"] = 0
         DATA["weeks"][week_timestamp]["issues"]["total"] = 0
+
         DATA["weeks"][week_timestamp]["pull_requests"] = {}
         DATA["weeks"][week_timestamp]["pull_requests"]["open"] = 0
         DATA["weeks"][week_timestamp]["pull_requests"]["total"] = 0
         DATA["weeks"][week_timestamp]["pull_requests"]["closed"] = 0
+
         DATA["weeks"][week_timestamp]["commits"] = {}
         DATA["weeks"][week_timestamp]["commits"]["total"] = week["total"]
+
+        DATA["weeks"][week_timestamp]["releases"] = {}
+        DATA["weeks"][week_timestamp]["releases"]["count"] = 0
+        DATA["weeks"][week_timestamp]["releases"]["latest"] = {}
+        DATA["weeks"][week_timestamp]["releases"]["latest"]["date"] = utc.localize(datetime.datetime.fromtimestamp(0))
+        DATA["weeks"][week_timestamp]["releases"]["latest"]["tag_name"] = ""
+        DATA["weeks"][week_timestamp]["releases"]["latest"]["id"] = ""
 
     print("Getting additions and deletions...")
     code_frequency = call_github_api("/stats/code_frequency")
@@ -166,6 +180,39 @@ if __name__ == "__main__":
         else:
             print("Skipping week " + str(week_ts) + " because it is out of range")
 
+    print("Getting releases...")
+
+    page = 0
+    while True:
+        print("Loading page " + str(page+1))
+        releases = call_github_api("/releases?page=" + str(page))
+
+        if len(releases) <= 0:
+            break
+
+        for release in releases:
+            print("Processing release " + str(release["tag_name"]) + " (" + str(release["id"]) + ")")
+
+            count_w = 0
+            for week in DATA["weeks"]:
+                release_created_dt = dateutil.parser.parse(release["created_at"])
+                week_dt = utc.localize(datetime.datetime.fromtimestamp(int(week)))
+
+                if release_created_dt < week_dt:
+                    DATA["weeks"][week]["releases"]["count"] = DATA["weeks"][week]["releases"]["count"] + 1
+
+                    if release_created_dt > DATA["weeks"][week]["releases"]["latest"]["date"]:
+                        DATA["weeks"][week]["releases"]["latest"]["date"] = release_created_dt
+                        DATA["weeks"][week]["releases"]["latest"]["tag_name"] = release["tag_name"]
+                        DATA["weeks"][week]["releases"]["latest"]["id"] = release["id"]
+
+                    count_w = count_w + 1
+
+            print("Release added " + str(count_w) + " times")
+
+        print("Added " + str(len(releases)) + " releases for page " + str(page+1))
+        page = page + 1
+
     print("Getting pull request issues...")
 
     populate_issues("pulls", "pull_requests", False)
@@ -173,6 +220,11 @@ if __name__ == "__main__":
     print("Getting regular issues...")
 
     populate_issues("issues", "issues", True)
+
+    print("Sanitizing data...")
+
+    for week in DATA["weeks"]:
+        DATA["weeks"][week]["releases"]["latest"]["date"] = DATA["weeks"][week]["releases"]["latest"]["date"].timestamp()
 
     print("Writing output...")
 
